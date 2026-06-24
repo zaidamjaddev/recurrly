7aeeaa9986e59cca2fda29b7da2bfb123759c86a
Zaid Amjad <h.zaidamjad29@gmail.com>
2026-06-24 21:46:05 +0500

    add tabs in bottom navigation

---
+++ app/(tabs)/insights.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const insights = () => {
+  return (
+    <View>
+      <Text>insights</Text>
+    </View>
+  )
+}
+
+export default insights
---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const subscriptions = () => {
+  return (
+    <View>
+      <Text>subscriptions</Text>
+    </View>
+  )
+}
+
+export default subscriptions
